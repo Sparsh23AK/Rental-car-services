5ab787f090cbf4dc3c53e50c28c0c088814176e3
Sparsh23AK <sparshgiri23@gmail.com>
2024-01-11 20:01:16 +0530

    sign In route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import errorHandler from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
 
-const signup = async (req, res, next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const userDetail = new User({ username, email, password: hashedPassword });
@@ -14,4 +16,22 @@ const signup = async (req, res, next) => {
   }
 };
 
-export default signup;
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const userDetail = await User.findOne({ email: email });
+    if (!userDetail) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, userDetail.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid Credentials!"));
+    const token = jwt.sign({ id: userDetail._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = userDetail._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1hr
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
