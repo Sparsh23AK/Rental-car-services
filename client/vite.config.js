01996f094388b03a7e2d1da4db8e2290b6017916
Sparsh23AK <sparshgiri23@gmail.com>
2024-03-07 21:40:20 +0530

    config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "http://localhost:3000",
+        target: "https://carentalv-1.onrender.com",
         changeOrigin: true,
       },
     },
