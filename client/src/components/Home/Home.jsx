6efe085e7cb382de84e8c8aa641e569b4c87c688
Sparsh23AK <sparshgiri23@gmail.com>
2024-03-08 16:42:09 +0530

    changing header format

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -34,22 +34,30 @@ export default function Home() {
   useEffect(() => {
     fetchData(); // Initial data fetch when the component mounts
     fetchBrands(); // Fetching brands
+    setElectricCars(() => cars.filter((car) => car.fuelType === "Electric"));
+    setUpcomingCars(() => cars.filter((car) => car.isUpcoming === true));
+    setTrendingCars(() => cars.filter((car) => car.isTrending === true));
   }, []);
 
   const fetchData = async () => {
     try {
       dispatch(fetchCarsStart());
-      const response = await fetch("/api/cars/getCars", {
+      await fetch("/api/cars/getCars", {
         method: "GET",
-        headers:{
-          'Content-Type': 'application/json',
-        }
-      });
-      const data = await response.json();
-      dispatch(fetchCarsSuccess(data));
-      setElectricCars(() => cars.filter((car) => car.fuelType === "Electric"));
-      setUpcomingCars(() => cars.filter((car) => car.isUpcoming === true));
-      setTrendingCars(() => cars.filter((car) => car.isTrending === true));
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => response.json())
+      .then((data) => {
+        dispatch(fetchCarsSuccess(data));
+        setLoading(false);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+      // console.log(data);
+      // const data = await response.json();
+      // dispatch(fetchCarsSuccess(data));
+
       setLoading(false);
     } catch (error) {
       dispatch(fetchCarsFailure(error));
@@ -62,9 +70,9 @@ export default function Home() {
       dispatch(fetchBrandStart());
       const response = await fetch("/api/cars/getBrands", {
         method: "GET",
-        headers:{
-          'Content-Type': 'application/json'
-        }
+        headers: {
+          "Content-Type": "application/json",
+        },
       });
       const data = await response.json();
       if (data.success === false) {
